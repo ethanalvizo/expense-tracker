6bb0d15ae1a850f800b52158f0ef4a8db047a21e
ethanalvizo <55671206+ethanalvizo@users.noreply.github.com>
2021-04-23 16:14:58 -0400

    Created login UI

---
+++ src/components/Forms/Login.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import { 
+    TextField,
+    Grid,
+    Card,
+    Button,
+    makeStyles 
+}  from '@material-ui/core';
+
+const Login = () => {
+    const [values, setValues] = useState({
+        email: '',
+        password: '',
+    })
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleFieldChange = (e) => {
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        })
+        console.log(values)
+    }
+
+    const handleSignIn = async () => {  
+        console.log(values, "signing in")
+    }
+
+    return (
+        <>
+        <Grid
+            container
+            direction="column"
+            alignItems="center"
+            justify="center"
+            style={{minHeight: '100vh'}}
+        >
+            <Card style={{minWidth: '30vw'}}>
+                <Grid
+                    container
+                    direction="column"
+                    justify="center"
+                    alignItems="center"
+                    spacing={3}
+                    style={{ padding: '2em'}}
+                >
+                    <Grid item>
+                        <h2>Login</h2>
+                    </Grid>
+                    <Grid item style={{width: "100%"}}>
+                        <TextField 
+                            name="email"
+                            type="text"
+                            placeholder="Email"
+                            value={values.email}
+                            onChange={handleFieldChange}
+                            variant="outlined"
+                            label="Username/Email"
+                            fullWidth={true}
+                        />
+                    </Grid>
+                    <Grid item style={{width: "100%"}}>
+                        <TextField 
+                            name="password"
+                            type="text"
+                            placeholder="Password"
+                            value={values.password}
+                            onChange={handleFieldChange}
+                            variant="outlined"
+                            label="Password"
+                            fullWidth={true}
+                        />
+                    </Grid>
+                    <Grid item style={{width: "100%"}}>
+                        <Button variant="contained" color="primary" fullWidth={true} onClick={handleSignIn}>Login</Button>
+                    </Grid>
+
+                </Grid>
+            </Card>
+
+        </Grid>
+        </>
+    )
+}
+
+export default Login
