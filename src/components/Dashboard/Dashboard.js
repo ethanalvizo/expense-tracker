783491b2cbc84129fd41d514fc7318e88690996f
ethanalvizo <55671206+ethanalvizo@users.noreply.github.com>
2021-04-24 17:58:50 -0400

    Moved db info call to dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,14 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useAuth } from '../../contexts/AuthContext';
+import db from '../../config/fire';
+
 import Transaction from './Transaction';
 import DataFeed from './DataFeed';
 import Trends from './Trends';
 
-export default function Dashboard() {
+const Dashboard = () => {
+    const [expenses, setExpenses] = useState([]); //expenses is similar to data variable but without the id
+    const [amount, setAmount] = useState({
+        expenses: 0,
+        income: 0
+    });
+    const { currentUser } = useAuth();
+
+    useEffect(() => {
+        const expenseRef = db.database().ref(`Transactions/${currentUser.uid}/2021/April`);
+        expenseRef.on('value', (items) => {
+            const expenses = items.val();
+
+            let expenseList = [], expenseSum = 0, incomeSum = 0
+            for (let id in expenses) {
+                expenseList.push({key: id, ...expenses[id]})
+                if (expenses[id].type === 'Expense') {
+                    expenseSum += expenses[id].amount
+                }
+                else {
+                    incomeSum += expenses[id].amount
+                }
+            }
+
+            expenseSum = expenseSum.toFixed(2);
+            incomeSum = incomeSum.toFixed(2);
+
+            setAmount({
+                expenses: expenseSum,
+                income: incomeSum
+            })
+            setExpenses(expenseList);
+        })
+    }, [currentUser.uid]);
+
     return (
         <>
             <div className="row my-5">
                 <div className="col-7 mx-auto">
-                    <DataFeed />
+                    <DataFeed expenses={expenses} amount={amount}/>
                 </div>
                 <div className="col-5 mx-auto my-auto" style={{ maxWidth: '600px' }}>
                     <Transaction />
@@ -26,4 +63,6 @@ export default function Dashboard() {
         </>
     )
 }
+
+export default Dashboard
 
--- src/components/Dashboard/DataFeed.js
+++ src/components/Dashboard/DataFeed.js
@@ -1,64 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { useAuth } from '../../contexts/AuthContext';
-import db from '../../config/fire';
 
-import { 
+import {
     Card,
     Badge
 } from 'react-bootstrap'
 import BootstrapTable from 'react-bootstrap-table-next';
 import paginationFactory from 'react-bootstrap-table2-paginator'
 
-const DataFeed = () => {
-    const [expenses, setExpenses] = useState([]); //expenses is similar to data variable but without the id
-    const [amount, setAmount] = useState({
-        expenses: 0,
-        income: 0
-    });
-    const [data, setData] = useState([]);
-    const { currentUser } = useAuth();
-
-    useEffect(() => {
-        const expenseRef = db.database().ref(`Transactions/${currentUser.uid}/2021/April`);
-        expenseRef.on('value', (items) => {
-            const expenses = items.val();
-
-            let expenseList = [], expenseSum = 0, incomeSum = 0
-            for (let id in expenses) {
-                expenseList.push(expenses[id])
-                if(expenses[id].type === 'Expense'){
-                    expenseSum += expenses[id].amount
-                }
-                else {
-                    incomeSum += expenses[id].amount
-                }
-            }
-
-            expenseSum = expenseSum.toFixed(2);
-            incomeSum = incomeSum.toFixed(2);
-
-            setAmount({
-                expenses: expenseSum,
-                income: incomeSum
-            })
-            setExpenses(expenseList);
-
-            setData([])
-            for (let index in expenses){
-                setData( prevState => [
-                    ...prevState, {
-                        'id': index,
-                        'name': expenses[index].name,
-                        'amount': expenses[index].amount,
-                        'category': expenses[index].category,
-                        'type': expenses[index].type,
-                        'date': expenses[index].date
-                    }
-                ])
-            }
-        })
-    }, [currentUser.uid]);
-
+const DataFeed = ({expenses, amount}) => {
     const columns = [{
         dataField: 'name',
         text: 'Name',
@@ -82,19 +31,19 @@ const DataFeed = () => {
     }
 
     ];
-
+    
     return (
         <>
             <Card className="border-0 shadow">
                 <Card.Body>
                     <h3 className="text-center mb-4 p-2 bg-light">Recent Expenses</h3>
                     <div className="row d-flex justify-content-around">
-                        <h5>Monthly Expenses <Badge variant="light">{amount.expenses}</Badge></h5>
-                        <h5>Monthly Income <Badge variant="light">{amount.income}</Badge></h5>
+                        {amount && <h5>Monthly Expenses <Badge variant="light">{amount.expenses}</Badge></h5>}
+                        {amount && <h5>Monthly Income <Badge variant="light">{amount.income}</Badge></h5>}
                     </div>
-                    <BootstrapTable bootstrap4 keyField='id' data={data} columns={columns} pagination={paginationFactory()} />
+                    <BootstrapTable bootstrap4 keyField='id' data={expenses} columns={columns} pagination={paginationFactory()} />
                 </Card.Body>
-                
+
             </Card>
 
         </>
